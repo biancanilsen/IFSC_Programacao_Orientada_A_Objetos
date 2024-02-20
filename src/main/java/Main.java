7c62924c8b1472b21e04569017cdedc526f2e009
biancanilsen <biancanilsen.entra21@gmail.com>
2024-02-20 19:53:22 -0300

    Hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+
+//    package - vai compilar e cria o diretório target
+//    clean - deleta o target (ás vezes dentro do target tem arquivos alterados e dá problema)
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
