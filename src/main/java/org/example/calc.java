34a0f195ad521ea188d52d31505f316c1ef50384
biancanilsen <biancanilsen.entra21@gmail.com>
2024-02-20 21:30:00 -0300

    Classe count

---
+++ src/main/java/org/example/calc.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class calc {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int res = a/b;
+        System.out.printf("%s+%s=%s \n", a,b,(a+b));
+        System.out.printf("%s-%s=%s \n", a,b,(a-b));
+        // Esse resultado irá apresentar 2 (sem decimal)
+        System.out.printf("%s/%s=%s \n", a,b, (a/b));
+        // Esse resultado irá apresentar 2 (com decimal)
+        System.out.printf("%s/%s=%s \n", a,b, ((float)a/b));
+        System.out.printf("%s*%s=%s \n", a,b,(a*b));
+
+        Double valor = 2d;
+        Double res1 = valor*2;
+        System.out.printf("Resultado" +res1);
+    }
+}
